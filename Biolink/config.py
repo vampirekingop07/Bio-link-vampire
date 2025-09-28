bc5763de6ddd5c9b25f8e8d98e569d2ad880bca9
vampirekingop07 <naaztravels62@gmail.com>
2025-09-28 16:18:01 +0530

    Update config.py

--- Biolink/config.py
+++ Biolink/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
+API_ID = int(os.getenv("API_ID", 12345))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID", 8410426172))
+OWNER_ID = int(os.getenv("OWNER_ID", 8486348579))
 MONGO_URI = os.getenv("MONGO_URI")
